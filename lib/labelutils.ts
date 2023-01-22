bf2f66f6a06fd62d5776cab9b3a996b8390cd24d
hiroshi <hiroshi3110@gmail.com>
2023-01-22 21:00:50 +0900

    Combination of labels for grouping instead of a single label.

--- lib/labelutils.ts
+++ lib/labelutils.ts
@@ -2,57 +2,50 @@ type Labels = { [k: string]: boolean };
 type Item = { _id: string; labels: Labels };
 type Grouped = { heading: string } | Item;
 
-export default {
-  serialize: (labels: Labels) => {
-    let list = [];
-    for (let label in labels) {
-      if (labels[label]) {
-        list.push(label);
-      } else {
-        list.push("!" + label);
-      }
+const serialize = (labels: Labels, opts: { sort?: boolean } = {}) => {
+  let list = [];
+  for (let label in labels) {
+    if (labels[label]) {
+      list.push(label);
+    } else {
+      list.push("!" + label);
     }
-    return list.join(" ");
-  },
-
-  deserialize: (str: string, opts: { ignoreNegate?: boolean } = {}) => {
-    let labels: Labels = {};
-    str.split(/\s+/).forEach((label) => {
-      if (label !== "") {
-        const m = label.match(/^(\!)?(\w+)$/);
-        if (m) {
-          if (!opts.ignoreNegate || !m[1]) {
-            labels[m[2]] = !m[1];
-          }
-        }
-      }
-    });
-    return labels;
-  },
-
-  autoGroup: (items: Item[]) => {
-    const labelsItems: { [k: string]: Item[] } = {};
-
-    items.forEach((item) => {
-      for (let label in item.labels) {
-        if (label in labelsItems) {
-          labelsItems[label].push(item);
-        } else {
-          labelsItems[label] = [item];
-        }
-      }
-    });
-    // console.log(labelsItems);
-
-    let results: Array<Grouped> = [];
-    for (let label in labelsItems) {
-      // console.log(label);
-      // console.log(labelsItems[label]);
-      if (labelsItems[label].length < items.length) {
-        results.push({ heading: label });
-        results = results.concat(labelsItems[label]);
-      }
-    }
-    return results;
-  },
+  }
+  return opts.sort ? list.sort().join(" ") : list.join(" ");
 };
+
+const deserialize = (str: string, opts: { ignoreNegate?: boolean } = {}) => {
+  let labels: Labels = {};
+  str.split(/\s+/).forEach((label) => {
+    if (label !== "") {
+      const m = label.match(/^(\!)?(\w+)$/);
+      if (m) {
+        if (!opts.ignoreNegate || !m[1]) {
+          labels[m[2]] = !m[1];
+        }
+      }
+    }
+  });
+  return labels;
+};
+
+const autoGroup = (items: Item[]) => {
+  const labelsItems: { [k: string]: Item[] } = {};
+  items.forEach((item) => {
+    const labels: string = serialize(item.labels, { sort: true });
+    if (labels in labelsItems) {
+      labelsItems[labels].push(item);
+    } else {
+      labelsItems[labels] = [item];
+    }
+  });
+  // console.log(labelsItems);
+  let results: Array<Grouped> = [];
+  for (let labels in labelsItems) {
+    results.push({ heading: labels });
+    results = results.concat(labelsItems[labels]);
+  }
+  return results;
+};
+
+export default { serialize, deserialize, autoGroup };
